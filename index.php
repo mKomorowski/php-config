03f2b4d7e3d1a424c9ae35327ff6a2ef5a15ccb2
michal <prozac202@gmail.com>
2015-06-13 00:53:43 +0100

    phpunit clover coverage

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    require __DIR__.'/vendor/autoload.php';
-
-    $configLoader = new mKomorowski\Config\Loader(__DIR__.'/tests/config');
-
-    //$config = new mKomorowski\Config\Config($configLoader, new mKomorowski\Config\Hosts);
